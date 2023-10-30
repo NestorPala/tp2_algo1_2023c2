edaaef82132a4c5c9fe2bf34bcaa14b4b839dbf5
NestorPala <npalavecino@fi.uba.ar>
2023-10-30 02:27:41 -0300

    added movies data and posters

--- data/snacks.js
+++ data/snacks.js
@@ -1,4 +1,8 @@
 module.exports = snacks = {
     "doritos": 2500.00,
-    "popcorn_xxl": 4300.00
+    "popcorn_xl": 3000.00,
+    "popcorn_xxl": 4300.00,
+    "papas_fritas": 1800.00,
+    "coca_cola_xl": 1500.00,
+    "coca_cola_xxl": 2350.00
 }
