3ec40caaf375d904ac3263f5babec66e702f1310
NestorPala <npalavecino@fi.uba.ar>
2023-10-31 15:17:46 -0300

    added more cinemas data

--- data/cinemas.js
+++ data/cinemas.js
@@ -2,21 +2,39 @@ const cinemas = [
     {
         "cinema_id": "1",
         "location": "Caballito",
-        "rooms": 7,
-        "seats_for_each_room": ["a1","a2","a3","b1","b2","b3"]
+        "available_seats": 32,
     },
     {
         "cinema_id": "2",
         "location": "Abasto",
-        "rooms": 8,
-        "seats_for_each_room": ["a1","a2","a3","b1","b2","b3","c1","c2","c3"]
+        "available_seats": 40,
     },
     {
         "cinema_id": "3",
         "location": "Puerto Madero",
-        "rooms": 5,
-        "seats_for_each_room": ["a1","a2","a3","a4","b1","b2","b3","b4"]
-    }
+        "available_seats": 25,
+    },
+    {
+        "cinema_id": "4",
+        "location": "Villa del parque",
+        "available_seats": 28,
+    },
+    {
+        "cinema_id": "5",
+        "location": "Palermo",
+        "available_seats": 36,
+    },
+    {
+        "cinema_id": "6",
+        "location": "Liniers",
+        "available_seats": 24,
+    },
+    {
+        "cinema_id": "7",
+        "location": "Olivos",
+        "available_seats": 30,
+    },
+    
 ]
 
 const cinemaHasMovieData = [
@@ -31,7 +49,24 @@ const cinemaHasMovieData = [
     {
         "cinema_id": "3",
         "has_movies": ["7", "8", "9", "14", "15", "17", "18", "19", "20", "25", "26"]
-    }
+    },
+    {
+        "cinema_id": "4",
+        "has_movies": ["1", "4", "5", "6", "7", "16", "17", "18", "19", "20", "21", "25"]
+    },
+    {
+        "cinema_id": "5",
+        "has_movies": ["2", "3", "9", "10", "11", "12", "13", "21", "22", "23", "24"]
+    },
+
+    {
+        "cinema_id": "6",
+        "has_movies": ["3", "5", "7", "8", "9", "11", "12", "19", "20", "22", "25", "26"]
+    },
+    {
+        "cinema_id": "7",
+        "has_movies": ["4", "10", "14", "15", "17", "18", "19", "20", "22", "26"]
+    },
 ]
 
 function cinemaHasMovie(movie_id) {
